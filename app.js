f6d5280b88195a972bf31ea9fd941582e611e961
Mary Martinez <mary.diana.martinez@gmail.com>
2022-02-09 14:40:04 -0800

    details pages working

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ const dogListContainer = document.getElementById('dog-list-container');
 // on load
 // fetch all dogs
 // render and append all dog cards to the container
+window.addEventListener('load', async () => {
+    const dogList = await getDogs();
+    dogListContainer.textContent = '';
+    // console.log(dogList);
+    for (let dog of dogList) {
+        const dogInfo = renderDogCard(dog);
+        dogListContainer.append(dogInfo);
+        console.log(dog);
+    }
+})
--- detail/detail.js
+++ detail/detail.js
@@ -3,6 +3,14 @@ import { renderDogDetail } from '../render-utils.js';
 
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
+window.addEventListener('load', async () => {
+    console.log('entering detail event listener');
+    const params = new URLSearchParams(window.location.search);
+    const selectedDog = await getDog(params.get('id'));
+    const showDog = renderDogDetail(selectedDog);
+    dogDetailContainer.append(showDog);
+
+})
 // on load
 // get the id from URL
 // use the id to fetch the dog
