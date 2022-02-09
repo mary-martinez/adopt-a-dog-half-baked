c870576e451ae1c7f4c54407f84b7ba6f57c29d2
Mary Martinez <mary.diana.martinez@gmail.com>
2022-02-09 14:48:31 -0800

    cleaning up code

--- detail/detail.js
+++ detail/detail.js
@@ -4,14 +4,9 @@ import { renderDogDetail } from '../render-utils.js';
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
 window.addEventListener('load', async () => {
-    console.log('entering detail event listener');
     const params = new URLSearchParams(window.location.search);
     const selectedDog = await getDog(params.get('id'));
     const showDog = renderDogDetail(selectedDog);
     dogDetailContainer.append(showDog);
 
 })
-// on load
-// get the id from URL
-// use the id to fetch the dog
-// render and append this dog's details to the container
